0704110ffd33885f3ddf1ea1a6184436c1ebf767
Levan Kusiani <46822531+LevanKusiani@users.noreply.github.com>
2023-03-10 01:01:19 +0400

    added color theme change option (#1)

--- src/components/metronome/Beat.js
+++ src/components/metronome/Beat.js
@@ -1,26 +1,36 @@
-import "./Beat.css";
+import styles from "./Beat.module.css";
 
-const Beat = ({beat, onWheelScroll}) => {
-    const createItems = (val) =>
-        {
-            let list = [];
+const Beat = ({ beat, isDark, onWheelScroll }) => {
+  const createItems = (val) => {
+    let list = [];
 
-            for(let i = 0; i < 8; i++){
-                const clsName = 'beat-step' + (i < val ? '' : '-disabled');
-                list.push(<div className={clsName}></div>);
-            }
+    for (let i = 0; i < 8; i++) {
+        if(i < val){
+            list.push(<div className={`${styles['beat-step']} ${isDark && styles.dark}`}></div>);
+        }
+        else{
+            list.push(<div className={`${styles['beat-step-disabled']} ${isDark && styles.dark}`}></div>);
+        }
+    //   const clsName = "beat-step" + (i < val ? "" : "-disabled");
+    //   list.push(<div className={`${clsName} ${isDark && styles.dark}`}></div>);
+    }
 
-            return list;
-        };
+    return list;
+  };
 
-    return (<div>
-        <div className='beat'>
-            <h1 onWheel={(event) => onWheelScroll(event)}>{beat}</h1>
-        </div>
-        <div onWheel={(event) => onWheelScroll(event)} className='beat-container'>
-            {createItems(beat)}
-        </div>
-    </div>);
-}
+  return (
+    <div>
+      <div className={`${styles.beat}`}>
+        <h1 onWheel={(event) => onWheelScroll(event)}>{beat}</h1>
+      </div>
+      <div
+        onWheel={(event) => onWheelScroll(event)}
+        className={`${styles["beat-container"]}`}
+      >
+        {createItems(beat)}
+      </div>
+    </div>
+  );
+};
 
 export default Beat;
