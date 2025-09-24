20cefdabadac0577d5e02534e1519cae77201a02
levankusiani <levan.kusiani@andromeda-health.com>
2025-09-24 14:50:41 +0400

    Add GitHub Pages deployment setup

---
+++ docs/metronomeWorker.js
@@ -0,0 +1,35 @@
+let intervalId; // reference to the setInterval invocation
+let interval = 100;
+
+onmessage = (event) => {
+  if(event.data.command){
+    switch(event.data.command){
+      case "play": {
+        intervalId = setInterval(() => {
+          postMessage("tick");
+        }, interval);
+        
+        break;
+      }
+      case "stop": {
+        clearInterval(intervalId);
+        intervalId = null;
+        
+        break;
+      }
+      default:
+        // Invalid command name
+    }
+  }
+
+  if (event.data.interval) {
+    interval = event.data.interval;
+
+    if(intervalId){
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        postMessage("tick");
+      }, interval);
+    }
+  }
+};
